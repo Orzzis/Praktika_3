4b8e70dae9dc6e032059cd71dc3fff3c927d09c4
Orzzis <kirill72tyum@gmail.com>
2023-06-10 04:09:24 +0500

    Добавил решение 1 задачи!

---
+++ Задача 1/Задача 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите количество целых положительных числе:");
+int count = int.Parse(Console.ReadLine());
+int i = 1;
+int sum = 0;
+while(count >= i)
+{
+    sum += i;
+    i++;
+}
+Console.WriteLine($"Сумма первых {count} целых положительных чисел равна {sum}");
